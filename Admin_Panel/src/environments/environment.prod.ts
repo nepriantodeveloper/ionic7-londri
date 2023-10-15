b08b59a70e50f42fa54386a7c92193516f78ecfd
nepriantodeveloper <neprianto@gmail.com>
2023-10-15 17:05:01 +0700

    komit 2

---
+++ Admin_Panel/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Washing Wala Full App Ionic 6 Capacitor
+  This App Template Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2022-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseUrl: 'https://your-api-url.com/public/api/',
+  imageUrl: 'https://your-api-url.com/public/storage/images/',
+};
---
+++ Owner_app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://your-api-url.com/public/',
+};
