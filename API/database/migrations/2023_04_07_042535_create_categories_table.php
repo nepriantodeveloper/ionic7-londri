55d3543d791a3688844c8e481ca10cd2487e7e72
nepriantodeveloper <neprianto@gmail.com>
2023-10-15 17:04:16 +0700

    komit pertama

---
+++ API/database/migrations/2023_04_07_042535_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('cover');
+            $table->text('descriptions')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->text('extra_field')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
